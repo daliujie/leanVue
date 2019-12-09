56f2fb86dc359b5a5341160c0ec09c0c6b89ed72
dajiege <1518148736@qq.com>
2019-12-09 18:27:43 +0800

    测试第一次提交

---
+++ 07-小案例/index.js
@@ -0,0 +1,57 @@
+let books = [
+	{
+		name: "大数据",
+		price: 78.89,
+		num:1,
+	},
+	{
+		name: "c++",
+		price: 63,
+		num:1,
+	},
+	{
+		name: "java",
+		price: 48,
+		num:1,
+	},
+	{
+		name: "js",
+		price: 19,
+		num:1,
+	},
+	{
+		name: "html",
+		price: 80,
+		num:1,
+	},
+]
+
+const app = new Vue({
+	el:"#bookTable",
+	data:{
+		books: books
+	},
+	methods:{
+		addBuyNum: function(book){
+			book.num ++
+		},
+		subBuyNum:function(book){
+			book.num -- ;
+		}
+	},
+	filters:{//定义过滤器，可以格式化
+//		tofixPrice: function(price){
+//			return price.toFixed(2); 
+//		}
+		tofixPrice: x => x.toFixed(2)
+	},
+	computed:{//定义所有的计算属性
+		totalPrice: function(){
+			return this.books.reduce(function(prev,cur){
+				return prev + cur.num * cur.price;
+			},0)
+		}
+		
+	}
+	
+})
